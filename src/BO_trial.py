8ff273213924710b7170b7f04075d387ffbaba44
zyyjjj <yz685@cornell.edu>
2022-01-01 18:25:07 -0500

    deprecate class() and use functions only

--- src/BO_trial.py
+++ src/BO_trial.py
@@ -62,12 +62,11 @@ def BO_trial(
             # or wrap this into a function and put into utils
             for _ in range(n_initial_pts):
                 # TODO: figure out how to sample hp config from search space
-                sampled_hp_config = None
+                sampled_hp_config = generate_initial_samples()
                 outcome = problem_evaluate(sampled_hp_config)
                 X = np.concatenate([X, sampled_hp_config], axis=0)
                 y.append(outcome)
             init_batch_id = 1
-        
 
     # fit initial model
     model, mll = fit_GP_model(X, y)    
@@ -99,6 +98,7 @@ def BO_trial(
 
         # if verbose: print stuff to STDOUT
         # also, save the newest X and y
+        # update init_batch_id to track how many iters so far
 
     best_all_trials.append(best_log)
 
@@ -107,6 +107,12 @@ def fit_GP_model(X, y):
 
     model = SingleTaskGP(X, y)
     mll = ExactMarginalLogLikelihood(model.likelihood, model)
+
+    # TODO: figure this out
+        # load state dict if it is passed
+        # if state_dict is not None:
+        # model.load_state_dict(state_dict)
+
     return model, mll
 
 
@@ -119,142 +125,20 @@ def suggest_new_pt(algo, X, y):
         acqf = ExpectedImprovement(model, best)
     
     # call optimize_acquisition_function_and_get_suggested_point()
+    # or optimize_acqf_mixed()
     # return new point
 
+def generate_initial_samples(n_samples, bounds):
+    # TODO: generate initial samples of hp_config
+    pass
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# --- below is deprecated
-
-
-
-class MyMethod():
-
-    # TODO: add a function to the Learner class that returns a "terminal" performance metric(s) of some sort
-
-    def __init__(self, problem, obj_func, path, num_trials, n_initial_pts, acqf_key, algo_key, search_space): # more attributes
-        # learner: maps input (e.g., hp config) to output(s) (e.g., validation loss / acc)
-        # path: where BO iteration data are saved
-
-        self.problem = problem
-        self.obj_func = obj_func
-        self.path = path
-        self.num_trials = num_trials
-        self.n_initial_pts = n_initial_pts
-        
-        if acqf_key == 'EI':
-            self.acqf = ExpectedImprovement()
-        # TODO: enable more (+ custom) acquisition function types
-        elif acqf_key == 'KG':
-            pass
-
-        # also specify what BO algorithm to use
-        # if algo_key == 'something':
-
-        self.X = None
-        self.y = []
-
-        # TODO: specify hp config search space -- how to do?
+"""
+On bounds of the search space:
+# TODO: specify hp config search space -- how to do?
         # first, specify bounds, sth like
         bounds = torch.tensor([[0.0] * problem.dim, [1.0] * problem.dim], **tkwargs)
 
-        # TODO: used to have 3 categories: base_config, self.opt_kwargs, self.loss_fn_kwargs
-        # include architecture config as 4th? or include in base_config
-    
-
-    def get_initial_data(self):
-        # get initial data for fitting the GP and likelihood parameters
-        
-        # either here, or in run(), enable restarting from available saved data
-
-
-        for _ in range(self.n_initial_pts):
-            # TODO: figure out how to sample hp config from search space
-            sampled_hp_config = None
-            outcome = self.problem_evaluate(sampled_hp_config)
-            self.X = np.concatenate([self.X, sampled_hp_config], axis=0)
-            self.y.append(outcome)
-        
-        
-    def fit_model(self):
-
-        model = SingleTaskGP(self.X, self.y)
-        mll = ExactMarginalLogLikelihood(model.likelihood, model)
-        # TODO: figure this out
-        # load state dict if it is passed
-        # if state_dict is not None:
-        # model.load_state_dict(state_dict)
-        
-        return mll, model
-
-
-    def evaluate_new_pt(self, new_pt):
-        # something like this
-        return self.problem.evaluate(new_pt)
-        
-        
-    def suggest_new_pt(self):
-        # given existing (X, Y), call optimize_acqf, return suggested values (And predictions?)
-
-        candidates, _ = optimize_acqf_mixed(
-            self.acqf,
-
-        )
-
-
-
-        pass
-
-    def run(self, MC_SAMPLES):
-
-        # first get initial data: X, y
-        self.get_initial_data()
-
-        # then fit an initial GP and mll using initial data
-        mll, model = self.fit_model()
-        best_all_trials = []
-        qmc_sampler = SobolQMCNormalSampler(num_samples=MC_SAMPLES) # or something else
-
-        for trial_idx in self.num_trials:
-
-            best = np.inf # or soemthing else
-            best_log = []
-            fit_gpytorch_model(mll)
-
-            # define acqf
-            if self.acqf_key == 'EI':
-                acqf = qExpectedImprovement(model, best, qmc_sampler, self.obj_func)
-
-            new_pt = self.suggest_new_pt(acqf)
-            new_y = self.evaluate_new_pt(new_pt)
-            X = torch.cat([X, new_pt]); y = torch.cat([y, new_y])
-
-            # log the best-performing ones so far (including random)
-            best_log.append(new_y)
-
-            # re-fit model
-            mll, model = self.fit_model(X, y)
-
-            # if verbose: print stuff to STDOUT
-            # also, save the newest X and y
-
-        best_all_trials.append(best_log)
-
-
-    # TODO: also simulate a random policy
-    # TODO: write evaluate(); implement more sophisticated early stopping; then look at KG
+# TODO: also simulate a random policy
+# TODO: write evaluate(); implement more sophisticated early stopping; then look at KG
+"""
