715248d6ccbf712dd4eb1970fe6b7c89b6fd2603
zyyjjj <yz685@cornell.edu>
2022-01-02 12:00:19 -0500

    add cifar cnn experiment runner

---
+++ experiment_problems/Cifar10_Cnn_runner.py
@@ -0,0 +1,27 @@
+from functools import partial
+from Cifar10_Cnn import HPs_to_VARY, problem_evaluate
+from experiment_manager import experiment_manager
+
+# Basic skeleton for running an experiment
+
+
+
+problem_name = 'Cifar10_Cnn'
+
+return_metrics = {'val_acc': 'max'}
+
+problem_evaluate_fixed_metrics = partial(problem_evaluate, return_metrics = return_metrics)
+
+algo = 'EI'
+
+experiment_manager(
+    problem = problem_evaluate_fixed_metrics,
+    problem_name = problem_name,
+    algo = algo,
+    first_trial = 1,
+    last_trial = 1,
+    n_initial_pts = 10,
+    n_bo_iter = 50,
+    restart = True,
+    input_dim = 5
+)
