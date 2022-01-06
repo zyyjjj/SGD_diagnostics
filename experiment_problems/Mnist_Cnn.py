79f3541486d7750da4ad968be6eba66f282081b5
zyyjjj <yz685@cornell.edu>
2022-01-06 10:13:43 -0500

    add experiment runner for Mnist CNN

--- experiment_problems/Mnist_Cnn.py
+++ experiment_problems/Mnist_Cnn.py
@@ -1,119 +1,137 @@
 import torch.nn as nn
-from utils.learner import Learner
 import torch
 from torch.utils.data import DataLoader, random_split
 from torch.optim import SGD, Adagrad, Adam
 import torchvision
 import torchvision.transforms as transforms
 import neptune.new as neptune
-from utils.callback import *
-import os
-
-# TODO: write problem_evaluate for MNIST
-class MnistCnnModel(nn.Module):
-    # TODO: allow customizing architecture, maybe include dropout rate, or change to MLP (simpler)
-    def __init__(self):
-        super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # output: 64 x 14 x 14
-
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2), # output: 128 x 7 x 7
-
-            nn.Flatten(), 
-            nn.Linear(128*7*7, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10))
-        
-    def forward(self, xb):
-        return self.network(xb)
+import os, sys
+sys.path.append('../')
+sys.path.append('/home/yz685/SGD_diagnostics/')
+from src.utils.learner import Learner
+from src.utils.callback import *
 
 
-
+HPs_to_VARY = {
+    'lr': ['uniform', [0.0001, 0.01]],
+    'log2_batch_size': ['int', [5, 10]],
+    'log2_aux_batch_size': ['int', [5, 10]],
+    'n_channels_1': ['int', [16, 64]],
+    'n_channels_2': ['int',  [16, 64]]
+}
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-def problem_evaluate(hp_config, problem_name, n_trials=1):
+train_frac = 0.7
+train_ds_whole = torchvision.datasets.MNIST('/home/yz685/SGD_diagnostics/experiments/mnist',
+                                train = True, download = True, transform = transforms.ToTensor())
+test_ds = torchvision.datasets.MNIST('/home/yz685/SGD_diagnostics/experiments/mnist',
+                                train = False, download = True, transform = transforms.ToTensor())
+
+train_size = int(len(train_ds_whole) * train_frac)  
+val_size = len(train_ds_whole) - train_size
+max_num_epochs = 300
+
+class MnistCnnModel(nn.Module):
+    # same architecture as that for Cifar-10, for comparison
+    def __init__(self, n_channels_1, n_channels_2):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels = 3, out_channels = n_channels_1, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels = n_channels_1, out_channels = n_channels_1, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2), # output is n_channels_1 * 16 * 16
+
+            nn.Conv2d(in_channels = n_channels_1, out_channels = n_channels_2, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels = n_channels_2, out_channels = n_channels_2, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2), # output is n_channels_2 * 8 * 8
+
+            nn.Conv2d(in_channels = n_channels_2, out_channels = n_channels_2, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels = n_channels_2, out_channels = n_channels_2, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2), # output is n_channels_2 * 4 * 4
+
+            nn.Flatten(),
+            nn.Linear(4 * 4 * n_channels_2, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+
+
+def problem_evaluate(X, return_metrics):
+    """
+    X is a tensor of size (num_trials, num_hps_to_vary)
+    return_metrics is a dictionary {'metric_name': 'return_type'}
+        where 'metric_name' in ['training_loss', 'val_loss', 'val_acc']
+        and 'return_type' in ['mean', 'last', 'max', 'min']
+    """
+
+    input_shape = X.shape
+
+    #print(input_shape, len(HPs_to_VARY))
+
+    assert input_shape[1] == len(HPs_to_VARY), \
+        'Input dimension 1 should match the number of hyperparameters to vary'
+
+    outputs = []
     
-    train_frac = 0.9
-    
-    # maybe pass in hp_config as a tuple
-    # then unpack it 
-    base_config, arch_config, opt_kwargs, loss_fn_kwargs = hp_config
+    # TODO: think about whether we want to set the same seed as the BO trial here
 
-    if problem_name == 'CIFAR-10':
-        transform = transforms.Compose(
-            [transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        train_ds_whole = torchvision.datasets.CIFAR10('/home/yz685/SGD_diagnostics/experiments/cifar',
-                                        train = True, download = True, transform = transform)
-        test_ds = torchvision.datasets.CIFAR10('/home/yz685/SGD_diagnostics/experiments/cifar',
-                                        train = False, download = True, transform = transform)
+    for i in range(input_shape[0]):
+        base_config = {'lr': X[i][0].item(),
+            'batch_size': 2**(X[i][1].item()),
+            'aux_batch_size': 2**(X[i][2].item())}        
+        hp_config = [base_config, {}, {}]
 
-        train_size = int(len(train_ds_whole) * train_frac)
-        val_size = len(train_ds_whole) - train_size
+        print('sampled config', X[i])
 
+        model = MnistCnnModel(int(X[i][3].item()), int(X[i][4].item()))
+        model.to(device)
+
+        optimizer = torch.optim.Adam
         train_ds, val_ds = random_split(train_ds_whole, [train_size, val_size])
 
-        run = neptune.init(
-            project="zyyjjj/SGD-diagnostics",
-            api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIwM2RkNWY2MS01MTU1LTQxNDMtOTE3Ni1mN2RlNjY5ZTQxNWUifQ==",
-            )
-
-        run["sys/tags"].add(['cifar'])  # organize things
-        run['params'] = hp_config
+        # run = neptune.init(
+        #     project="zyyjjj/SGD-diagnostics",
+        #     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIwM2RkNWY2MS01MTU1LTQxNDMtOTE3Ni1mN2RlNjY5ZTQxNWUifQ==",
+        #     )
+        # run["sys/tags"].add(['cifar'])  # organize things
+        # run['params'] = hp_config + [{'arch_parmas': [X[i][3].item(), X[i][4].item()]}]
             
-        model = CifarCnnModel(arch_config)
-        model.to(device)
-        loss_fn = torch.nn.functional.cross_entropy()
+        loss_fn = torch.nn.functional.cross_entropy
+        callbacks = [EarlyStopping(metric = 'val_acc', patience = 5, warmup = 20, to_minimize=False, tolerance_thresh=0.05)]
 
-    elif problem_name == 'MNIST':
-        train_ds_whole = torchvision.datasets.MNIST('/home/yz685/SGD_diagnostics/experiments/mnist',
-                                        train = True, download = True, transform = transforms.ToTensor())
-        test_ds = torchvision.datasets.MNIST('/home/yz685/SGD_diagnostics/experiments/mnist',
-                                        train = False, download = True, transform = transforms.ToTensor())
+        learner = Learner(hp_config, model, train_ds, val_ds, optimizer, loss_fn, callbacks)
+        logged_performance_metrics = learner.fit(max_num_epochs, device = device)
 
-        train_size = int(len(train_ds_whole) * train_frac)  
-        val_size = len(train_ds_whole) - train_size
+        output = []
+        for k,t in return_metrics.items():
+            if t == 'mean':
+                out = np.mean(logged_performance_metrics[k])
+            elif t == 'last':
+                out = logged_performance_metrics[k][-1]
+            elif t == 'max':
+                out = max(logged_performance_metrics[k])
+            elif t == 'min':
+                out = min(logged_performance_metrics[k])
+        
+            output.append(out)
 
-        train_ds, val_ds = random_split(train_ds_whole, [train_size, val_size])
-
-        run = neptune.init(
-            project="zyyjjj/SGD-diagnostics",
-            api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIwM2RkNWY2MS01MTU1LTQxNDMtOTE3Ni1mN2RlNjY5ZTQxNWUifQ==",
-            )
-
-        run["sys/tags"].add(['cifar'])  # organize things
-        run['params'] = hp_config
-            
-        model = MnistCnnModel(arch_config)
-        model.to(device)
-        loss_fn = torch.nn.functional.cross_entropy()
-
-    #str(args.optimizer)?
-    save_label = 'Adam' + \ 
-    '_'.join('{}_{}'.format(*p) for p in sorted(base_config.items())) + \
-    '_'.join('{}_{}'.format(*p) for p in sorted(arch_config.items())) + \
-    '_'.join('{}_{}'.format(*p) for p in sorted(opt_kwargs.items())) + \
-    '_'.join('{}_{}'.format(*p) for p in sorted(loss_fn_kwargs.items()))
-
-    script_dir = os.path.abspath(os.getcwd())
-    results_folder = script_dir + "/results/" + save_label + "/" + "trial_" + str(args.trial) + "/" 
-    os.makedirs(results_folder, exist_ok=True)
-
-    callbacks = [MetricsLogger(results_folder), EarlyStopping(metric = 'val_acc', patience = 5, warmup = 20, to_minimize=False, tolerance_thresh=0.05)]
-
-    # TODO: specify optimizer
-    learner = Learner(hp_config, model, train_ds, val_ds, optimizer, loss_fn, callbacks, run)
-
-    # TODO: call learner.fit() and return metric(s) of interest
+        outputs.append(output)
+    
+    # return a tensor of shape num_trials x len(return_metrics)
+    return torch.Tensor(outputs)
